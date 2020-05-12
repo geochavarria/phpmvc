edbcc9baa616ca43e83decf81a408899fcadfb46
Geo Chavarria <edwin20100@gmail.com>
2020-05-12 17:50:34 -0600

    Parte 5 - Controlador Desde la Url

--- app/library/Core.php
+++ app/library/Core.php
@@ -4,16 +4,34 @@
     //Productos->Update->M_1_PI
 
     class Core{
-        protected $currentController ="pages";
+        protected $currentController ="Pages";
         protected $currentMethod = "index.php";
         protected $parameters = [];
 
         public function __construct(){
             $url = $this->getUrl();
+
+            //Controller Exists
+            if(file_exists('../app/controller/'.ucwords($url[0].'.ctrl.php')))
+            {
+                $this->currentController = ucwords($url[0]);
+                
+                unset($url[0]);
+            }
+
+            require_once '../app/controller/'.$this->currentController.'.ctrl.php';
+            $this->currentController = new $this->currentController;
         }
 
         public function getUrl(){
-            echo $_GET['url'];
+           if(isset($_GET['url'])){
+                $url = rtrim($_GET['url'], '/');
+                //URL Validate
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/',$url);
+
+                return $url;
+            }
         }
     }
 ?>
